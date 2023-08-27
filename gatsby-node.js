a3b4379219b88df05160311d4bbba5f4a86d54bb
YujiTakebuchi <takeyou.swift@gmail.com>
2023-08-27 20:16:54 +0900

    Test fs dir search

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,23 @@
 // import path from "path";
 // import data from "./src/data/articles.json";
+const fs = require("fs");
 const path = require("path");
+
+fs.readdir("./src/data", (err, files) => {
+  console.log(err);
+  console.log(files);
+  //   files.forEach((file) => {
+  //     console.log(file);
+  //   });
+});
 const data = require("./src/data/articles.json");
 
 exports.createPages = ({ actions }) => {
   const { createPage } = actions;
   const templatePath = path.resolve("./src/templates/articles.jsx");
+  console.log(data.basename);
   data.forEach((articleObject) => {
-    const pathName = `articles/${articleObject.slug}`;
+    const pathName = `${articleObject.dir}/${articleObject.slug}`;
     createPage({
       path: pathName,
       component: templatePath,
