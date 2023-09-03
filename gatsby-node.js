42834881021196641a3395a1e1a597cee399a75b
YujiTakebuchi <takeyou.swift@gmail.com>
2023-09-04 00:05:44 +0900

    Generate template pages sync json directory

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,21 +3,38 @@
 const { glob } = require("glob");
 const path = require("path");
 
-glob("./src/data/**/*.json").then((data) => {
-  console.log(data);
-});
-const data = require("./src/data/articles.json");
+const templateCompPathMap = {
+  "src/data": "./src/templates/articles.jsx",
+  "src/data/articles": "./src/templates/articles.jsx",
+  "src/data/column": "./src/templates/articles.jsx",
+};
 
 exports.createPages = ({ actions }) => {
-  const { createPage } = actions;
-  const templatePath = path.resolve("./src/templates/articles.jsx");
-  console.log(data.basename);
-  data.forEach((articleObject) => {
-    const pathName = `${articleObject.dir}/${articleObject.slug}`;
-    createPage({
-      path: pathName,
-      component: templatePath,
-      context: articleObject,
+  glob("./src/data/**/*.json").then((data) => {
+    console.log(data);
+    data.forEach((tmp) => {
+      const templateBaseName = path.basename(tmp);
+      const pathDir = path.dirname(tmp);
+      console.log("templateBaseName: ", templateBaseName);
+      console.log("pathDir: ", pathDir);
+      console.log("map: ", templateCompPathMap[pathDir]);
+
+      const { createPage } = actions;
+      //   const templatePath = path.resolve("./src/templates/articles.jsx");
+      const templatePath = path.resolve(templateCompPathMap[pathDir]);
+      const jsonObj = require(`./${tmp}`);
+      console.log("jsonObj: ", jsonObj);
+      jsonObj.forEach((articleObject) => {
+        const pathName = `${pathDir.replace(/.*src\/data/, "")}/${
+          articleObject.slug
+        }`;
+        createPage({
+          path: pathName,
+          component: templatePath,
+          context: articleObject,
+        });
+      });
     });
   });
 };
+// const data = require("./src/data/articles.json");
