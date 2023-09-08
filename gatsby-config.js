5243289188b33a91f8492a8c1f63e663569bd4e9
YujiTakebuchi <takeyou.swift@gmail.com>
2023-09-08 20:29:44 +0900

    Modify indent

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@
 module.exports = {
   siteMetadata: {
     title: `official-getting-started`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-sass"]
+  plugins: ["gatsby-plugin-sass"],
 };
