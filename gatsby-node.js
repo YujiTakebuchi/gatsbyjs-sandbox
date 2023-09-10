122d274b19bf1035bf0e645dc1e48a7060829bbc
YujiTakebuchi <takeyou.swift@gmail.com>
2023-09-10 14:32:44 +0900

    Modify logic of createPage by Promise

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,24 +6,23 @@ const templateCompPathMap = {
   "src/data/column": "./src/templates/articles.jsx",
 };
 
-exports.createPages = ({ actions }) => {
-  glob("./src/data/**/*.json").then((data) => {
-    data.forEach((tmp) => {
-      const pathDir = path.dirname(tmp);
+exports.createPages = async ({ actions }) => {
+  const pathList = await glob("./src/data/**/*.json");
+  pathList.forEach((tmp) => {
+    const pathDir = path.dirname(tmp);
 
-      const { createPage } = actions;
-      const templatePath = path.resolve(templateCompPathMap[pathDir]);
-      const jsonObj = require(`./${tmp}`);
-      jsonObj.forEach((articleObject) => {
-        const pathName = `${pathDir.replace(/.*src\/data/, "")}/${
-          articleObject.slug
-        }`;
+    const { createPage } = actions;
+    const templatePath = path.resolve(templateCompPathMap[pathDir]);
+    const jsonObj = require(`./${tmp}`);
+    jsonObj.forEach((articleObject) => {
+      const pathName = `${pathDir.replace(/.*src\/data/, "")}/${
+        articleObject.slug
+      }`;
 
-        createPage({
-          path: pathName,
-          component: templatePath,
-          context: articleObject,
-        });
+      createPage({
+        path: pathName,
+        component: templatePath,
+        context: articleObject,
       });
     });
   });
