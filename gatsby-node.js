4b28c2a93668a637577c2c1f945ec64321e1805d
YujiTakebuchi <takeyou.swift@gmail.com>
2023-10-02 13:19:13 +0900

    Add logic of switch createPage structure dir or html automatically

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,23 +1,52 @@
 const { glob } = require("glob");
 const path = require("path");
 
+// const templateCompPathMap = {
+//   "src/data/articles": "./src/templates/articles.jsx",
+//   "src/data/column": "./src/templates/articles.jsx",
+// };
 const templateCompPathMap = {
-  "src/data/articles": "./src/templates/articles.jsx",
-  "src/data/column": "./src/templates/articles.jsx",
+  articles: "./src/templates/articles.jsx",
+  column: "./src/templates/articles.jsx",
 };
 
-exports.createPages = async ({ actions }) => {
-  const jsonPathList = await glob("./src/data/**/*.json");
+const createPagesByPathList = ({ jsonPathList, pageType, actions }) => {
+  // const jsonPathList = await glob("./src/data/**/*.json");
   jsonPathList.forEach((jsonPath) => {
     const pathDir = path.dirname(jsonPath);
 
     const { createPage } = actions;
-    const templatePath = path.resolve(templateCompPathMap[pathDir]);
+    const templateKey = (() => {
+      switch (pageType) {
+        case "dir":
+          return pathDir.replace(/.*src\/data\//, "");
+        case "html":
+          return pathDir.replace(/.*src\/html_pages\//, "");
+        default:
+          return undefined;
+      }
+    })();
+    console.log("templateKey: ", templateKey);
+    console.log("pathDir: ", pathDir);
+    const templatePath = path.resolve(templateCompPathMap[templateKey]);
+
     const jsonObj = require(`./${jsonPath}`);
     jsonObj.forEach((articleObject) => {
-      const pathName = `${pathDir.replace(/.*src\/data/, "")}/${
-        articleObject.slug
-      }`;
+      const pathName = (() => {
+        switch (pageType) {
+          case "dir":
+            return `${pathDir.replace(/.*src\/data/, "")}/${
+              articleObject.slug
+            }`;
+          case "html":
+            return `${pathDir.replace(/.*src\/html_pages/, "")}/${
+              articleObject.slug
+            }.html`;
+          default:
+            return undefined;
+        }
+      })();
+      console.log(pathName);
 
       createPage({
         path: pathName,
@@ -26,4 +55,56 @@ exports.createPages = async ({ actions }) => {
       });
     });
   });
+};
+
+exports.createPages = async ({ actions }) => {
+  const dirJsonPathList = await glob("./src/data/**/*.json");
+  createPagesByPathList({
+    jsonPathList: dirJsonPathList,
+    pageType: "dir",
+    actions,
+  });
+  // dirJsonPathList.forEach((jsonPath) => {
+  //   const pathDir = path.dirname(jsonPath);
+
+  //   const { createPage } = actions;
+  //   const templatePath = path.resolve(templateCompPathMap[pathDir]);
+  //   const jsonObj = require(`./${jsonPath}`);
+  //   jsonObj.forEach((articleObject) => {
+  //     const pathName = `${pathDir.replace(/.*src\/data/, "")}/${
+  //       articleObject.slug
+  //     }`;
+
+  //     createPage({
+  //       path: pathName,
+  //       component: templatePath,
+  //       context: articleObject,
+  //     });
+  //   });
+  // });
+
+  const htmlJsonPathList = await glob("./src/html_pages/**/*.json");
+  createPagesByPathList({
+    jsonPathList: htmlJsonPathList,
+    pageType: "html",
+    actions,
+  });
+  // htmlJsonPathList.forEach((jsonPath) => {
+  //   const pathDir = path.dirname(jsonPath);
+
+  //   const { createPage } = actions;
+  //   const templatePath = path.resolve(templateCompPathMap[pathDir]);
+  //   const jsonObj = require(`./${jsonPath}`);
+  //   jsonObj.forEach((articleObject) => {
+  //     const pathName = `${pathDir.replace(/.*src\/data/, "")}/${
+  //       articleObject.slug
+  //     }`;
+
+  //     createPage({
+  //       path: pathName,
+  //       component: templatePath,
+  //       context: articleObject,
+  //     });
+  //   });
+  // });
 };
